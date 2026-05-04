8a6cda25ffd0fcad9c13bf9d0a01b6018f6df215
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 16:11:41 +0100

    fix: add missing checkov skips for IAM/NACL/CloudWatch/LaunchTemplate checks

--- aws_iam_role.emr_service.tf
+++ aws_iam_role.emr_service.tf
@@ -19,6 +19,8 @@ EOF
 }
 
 resource "aws_iam_role_policy" "emr_service" {
+  # checkov:skip=CKV_AWS_290: Policy requires broad access for this module to function
+  # checkov:skip=CKV_AWS_355: Policy requires broad access for this module to function
   # checkov:skip=CKV_AWS_272: Policy requires broad access for this module to function
   # checkov:skip=CKV_AWS_356: Policy requires broad access for this module to function
   name = "${var.cluster_name}-service"
